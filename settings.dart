220ef2143b3f99e047ce1e972e0cedba1cbd4942
nbouzari1991-hue <n.bouzari1991@gmail.com>
2025-11-26 23:20:28 +0330

    Update settings.dart

--- settings.dart
+++ settings.dart
@@ -1,15 +1,1 @@
-import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('تنظیمات')),
-      body: ListView(children: [
-        ListTile(title: Text('آدرس سرور'), subtitle: Text('http://10.0.2.2:8000')),
-        ListTile(title: Text('نسخه اپ'), subtitle: Text('0.1.0')),
-        ListTile(title: Text('خروج'), onTap: ()=>Navigator.pushReplacementNamed(context, '/')),
-      ],),
-    );
-  }
-}
